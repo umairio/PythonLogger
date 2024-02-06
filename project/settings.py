26576e9ab57e7529c345c8e76a065547525e2d42
umairio <umairmateen55@gmail.com>
2024-02-06 20:31:48 +0500

    Abstract User Inheritence started

--- project/settings.py
+++ project/settings.py
@@ -90,6 +90,8 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = 'core.User'
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
